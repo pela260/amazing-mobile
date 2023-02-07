a159e70f462dce76850ce01f84ff350a8dfa0ce9
Pelayo <pela260@gmail.com>
2023-02-07 13:06:10 +0100

    estructura, atributos y constructor de la segunda clase

---
+++ Procesador.java
@@ -0,0 +1,15 @@
+/**
+ * Proyecto practicando el flujo de trabajo básico en colaboracion
+ */
+
+public class Procesador {
+    
+    private String modelo;
+    
+    private int potencia;
+    
+    public Procesador(String modelo, int potencia) {
+        this.modelo = modelo;
+        this.potencia = potencia;
+    }
+}
