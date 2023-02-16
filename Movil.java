715ddcba9db98dd034197f6a48a63b0da2d8184e
Diego de la Fuente <diegodelafuentealonso@gmail.com>
2023-02-16 11:00:47 +0100

    cuarto commit

---
+++ Movil.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Movil here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Movil
+{
+    // instance variables - replace the example below with your own
+    private String marca;
+    private boolean encendido;
+    private Procesador GPU;
+
+    /**
+     * Constructor for objects of class Movil
+     */
+    public Movil()
+    {
+        
+    }
+}
